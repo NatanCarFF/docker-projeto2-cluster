e0bd9edd910447e7163255e454eb742284cc2d35
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2024-04-05 16:27:53 -0300

    Update master.sh
    Atualização 050520241627

--- master.sh
+++ master.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo docker swarm init --advertise-addr=10.10.10.100
+sudo docker swarm init --advertise-addr=192.168.50.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+sudo apt-get update
+sudo apt-get install -y docker.io
